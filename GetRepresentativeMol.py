329f288a2e732f3fe8ae3edd1015247d5a0081a0
tamshun <tamura.shunsuke.tn0@ms.naist.jp>
2022-09-08 21:01:28 +0200

    rep mols

---
+++ GetRepresentativeMol.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+import os
+from openeye import oechem
+from rdkit.Chem import Draw
+from rdkit import Chem
+
+splits = ['wodirection_trtssplit', 'axv']
+balance = True
+mls = ['SVM', 'XGBoost', 'Random_Forest', 'FCNN', 'FCNN_separated', 'MPNN', 'MPNN_separated', '1NN', '5NN']
+
+for split in splits:
+    print(split)
+    for ml in mls:
+        print(ml)
+        logdir = './Log_%s/%s/' %(split, ml)
+        scoredir = './Score_%s/%s/' %(split, ml)
+
+        files = [f for f in os.listdir(logdir) if os.path.isfile(logdir+f)]
+        if split == 'axv':
+            files = [f for f in files if 'bothout' in f]
+        
+        #intialize
+        if ml not in ['1NN']:
+            file_ac, mmp_ac, prob_ac = None, None, -100
+            col = 'prob'
+        else:
+            file_ac, mmp_ac, prob_ac = None, None, 100
+            col = 'distance'
+        
+        file_nonac, mmp_nonac, prob_nonac = None, None, 100
+        
+        for file in files:
+            #ACs
+            log = pd.read_csv(logdir+file, sep='\t', index_col=0)
+            log = log[log['trueY']==1]
+            log = log[log['predY']==1]
+            
+            if log.shape[0] > 0:
+                if ml not in ['1NN']:
+                    prob = np.max(log[col])
+                    if prob > prob_ac:
+                        file_ac = file
+                        idx_ac  = log.index[np.argmax(log[col])]
+                        mmp_ac  = log.loc[idx_ac, 'ids']
+                        prob_ac = prob
+                else:
+                    prob = np.min(log[col])
+                    if prob < prob_ac:
+                        file_ac = file
+                        idx_ac  = log.index[np.argmin(log[col])]
+                        mmp_ac  = log.loc[idx_ac, 'ids']
+                        prob_ac = prob
+                    
+            #NonACs
+            log = pd.read_csv(logdir+file, sep='\t', index_col=0)
+            
+            if 0 in log['trueY'].tolist():
+                log = log[log['trueY']==0]
+                log = log[log['predY']==0]
+            else:
+                log = log[log['trueY']==-1]
+                log = log[log['predY']==-1]
+            
+            if log.shape[0] > 0:
+                prob = np.min(log[col])
+                if prob < prob_nonac:
+                    file_nonac = file
+                    idx_nonac  = log.index[np.argmin(log[col])]
+                    mmp_nonac  = log.loc[idx_nonac, 'ids']
+                    prob_nonac = prob
+
+        data = pd.read_csv('./Dataset/Data/%s.tsv' %(file_ac.split('_')[0]), sep='\t', index_col='id')
+        smi_core = data.loc[mmp_ac, 'core'].replace('R1', '*')
+        smi_sub1 = data.loc[mmp_ac, 'sub1'].replace('R1', '*')
+        smi_sub2 = data.loc[mmp_ac, 'sub2'].replace('R1', '*')
+
+        mol_core = Chem.MolFromSmiles(smi_core)
+        mol_sub1 = Chem.MolFromSmiles(smi_sub1)
+        mol_sub2 = Chem.MolFromSmiles(smi_sub2)
+
+        img = Draw.MolsToGridImage([mol_core, mol_sub1, mol_sub2], subImgSize=(400,200))
+        img.save(scoredir+'AC_%s_%s.png'%(file_ac.split('_')[0], mmp_ac))
+        
+        data = pd.read_csv('./Dataset/Data/%s.tsv' %(file_nonac.split('_')[0]), sep='\t', index_col='id')
+        smi_core = data.loc[mmp_nonac, 'core'].replace('R1', '*')
+        smi_sub1 = data.loc[mmp_nonac, 'sub1'].replace('R1', '*')
+        smi_sub2 = data.loc[mmp_nonac, 'sub2'].replace('R1', '*')
+
+        mol_core = Chem.MolFromSmiles(smi_core)
+        mol_sub1 = Chem.MolFromSmiles(smi_sub1)
+        mol_sub2 = Chem.MolFromSmiles(smi_sub2)
+
+        img = Draw.MolsToGridImage([mol_core, mol_sub1, mol_sub2], subImgSize=(400,200))
+        img.save(scoredir+'NonAC_%s_%s.png'%(file_nonac.split('_')[0], mmp_nonac))
